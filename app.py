af96689c3ccde98ae8808a27768f40a83a849841
haiquy0-boop <haiquy0@icloud.com>
2026-05-17 08:46:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -202,8 +202,8 @@ def start_master():
             "┗ /setdelay <s>"
         )
     )
-                    )
-                )
+                    
+                
 
             # ============ ADMIN PANEL ============
 
